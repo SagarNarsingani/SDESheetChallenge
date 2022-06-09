42bf09f80ac802ad3c9a5301bffb0bbbe6ac8556
Sagar Narsingani <sagarhn2411@gmail.com>
2022-06-09 23:26:40 +0530

    Day 2

--- Day 2 - Arrays/MergeSortedArrays.cpp
+++ Day 2 - Arrays/MergeSortedArrays.cpp
@@ -29,6 +29,27 @@ void MergeSortedArrays(vector<int> &arr1, vector<int> arr2, int m, int n){
     return;
 }
 
+// Time Complexity: O(m+n)
+void Merge(vector<int> &arr1, vector<int> arr2, int m, int n){
+    int i = m-1, j=n-1, k=m+n-1;
+    while(i>=0 and j>=0){
+        if(arr1[i]>=arr2[j]){
+            arr1[k--] = arr1[i--];
+        } else {
+            arr1[k--] = arr2[j--];
+        }
+    }
+
+    while(j>=0){
+        arr1[k--] = arr2[j--];
+    }
+    while(i>=0){
+        arr1[k--] = arr1[i--];
+    }
+
+    return;
+}
+
 int main(){
     vector<int> arr1 = {1, 4, 6, 10, 0, 0};
     vector<int> arr2 = {2,5};

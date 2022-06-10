0541994aaa0b67458804561a13753ea75557e342
Sagar Narsingani <sagarhn2411@gmail.com>
2022-06-10 18:27:43 +0530

    Day 3

---
+++ Day 3 - Arrays/ModularExp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Time Complexity: O(Log n),
+// Space Complexity: O(1).
+// Types of variables is the main thing to watch out for.
+int ModularExp(int x, int n, int m){
+    long long ans=1;
+    long long base = (long long) x;
+    base = base%m;
+
+    if(base==0) return 0;
+
+    while(n>0){
+
+        // if N is odd in X^N, we will handle X seprately and X^N-1 (N-1 will be even) saprately.
+        if(n&1)
+            ans = (ans*base)%m;
+        
+        base = (base*base)%m;
+        n = n >> 1;
+    }
+
+    return (int) ans%m;
+}
+
+int main(){
+    cout << ModularExp(10000000, 2, 10000001);
+    return 0;
+}

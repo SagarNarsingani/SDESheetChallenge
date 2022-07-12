4c223d471dd1a93ed65d4c0d734fae29e018f834
Sagar Narsingani <sagarhn2411@gmail.com>
2022-07-12 20:56:31 +0530

    Day 21

--- Day 20 - BST/BSTFromPre.cpp
+++ Day 20 - BST/BSTFromPre.cpp
@@ -3,7 +3,22 @@
 #include "BST.h"
 using namespace std;
 
-// Time Complexity: O(N).
+Node *BSTFromPre(vector<int> &arr, int &i, int upper){
+    // if node crosses the upper bound.
+    if(i==arr.size() || arr[i]>upper){
+        return NULL;
+    }
+
+    Node *node = new Node(arr[i++]);
+    // on the left side value of current node is the upper bound.
+    node->left = BSTFromPre(arr, i, node->data);
+    // on the right side the upper bound for current node is the upper bound.
+    node->right = BSTFromPre(arr, i, upper);
+
+    return node;
+}
+
+// Time Complexity: O(N*N).
 // Space Complexity: O(N).
 void addNode(Node *&root, int x){
     if(!root){
@@ -22,10 +37,11 @@ void addNode(Node *&root, int x){
 
 int main(){
     vector<int> pre = {10, 5, 1, 7, 40, 50};
-    Node *root=NULL;
-    for(int i: pre){
-        addNode(root, i);
-    }
+    int i = 0;
+    Node *root=BSTFromPre(pre, i, INT_MAX);
+    // for(int i: pre){
+    //     addNode(root, i);
+    // }
     traverse(root);
     return 0;
 }
